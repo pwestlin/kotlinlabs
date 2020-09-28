db889936615352e2b61b4c8c0828de8bdbbe0967
Peter Vestlin <peter.vestlin@lm.se>
2020-09-28 19:44:46 +0200

    Refact

--- intro/src/test/kotlin/nu/westlin/kotlin/learnkotlin/lessbasic/RunCatchingTest.kt
+++ intro/src/test/kotlin/nu/westlin/kotlin/learnkotlin/lessbasic/RunCatchingTest.kt
@@ -4,7 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
-internal class RunCatching {
+internal class RunCatchingTest {
 
     private class Service {
         fun doSomething(something: String?): String {
